80f2d00ada0ac70ca15838f1330ca27bd7b200cb
Varrium <rolandogarciaram@gmail.com>
2022-07-05 19:39:07 -0700

    Added changeGrid button functionality

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
-const div = document.createElement('div')
-const grid = document.querySelector('.grid')
-
+let squareQuant = 16;
+const div = document.createElement('div');
+const grid = document.querySelector('.grid');
+const changeGrid = document.querySelector('.changeGrid')
 const title = document.querySelector('.title')
 /* title.addEventListener('mouseover', () => {
     title.style.background = 'black'
 }); 
 */
+
+function askSquareQuan() {
+    squareQuant = prompt('Introduce the number of squares per line: ')    
+    if ((squareQuant > 100) || (squareQuant < 1 )) {
+        let alert = alert('Please type a number between 1 and 100')
+        console.log('no pass')
+        // como regresar reiniciar la funcion para volver a preguntar
+        } else {
+            console.log('yes pass')
+            return squareQuant
+        } 
+}
+
+function removeGrid(hoveredItems) {
+    hoveredItems = document.querySelectorAll('.hovered')
+    hoveredItems.forEach(hoveredItem => {
+        hoveredItem.remove();
+    })
+    
+}
+
 function changeBackground(div) {
     return div.style.background = 'rgb(2, 29, 36)'
 
 }
+
+
 
 function makeGrid (squareQuant) {
     let widthAndHeight = 480/squareQuant
@@ -19,14 +43,22 @@ function makeGrid (squareQuant) {
     for (let i = 0; i < (squareQuant**2); ++i) {
         grid.appendChild(div.cloneNode())
     }
-    };
-    
 
-makeGrid(16);
-let hoveredItems = document.querySelectorAll('.hovered')
-hoveredItems.forEach( hoveredItem => {
+    let hoveredItems = document.querySelectorAll('.hovered')
+    hoveredItems.forEach( hoveredItem => {
     hoveredItem.addEventListener('mouseover', () => {
         changeBackground(hoveredItem);
     })
+    });    
+    return hoveredItems
+    };
+    
 
-});    
+
+makeGrid(16);
+
+changeGrid.addEventListener('click', () =>{
+    askSquareQuan(); 
+    removeGrid();
+    makeGrid(squareQuant);
+} )
